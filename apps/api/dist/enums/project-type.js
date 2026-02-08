b7937bb40e4be363d614732a4fc0229c65b4dec3
Dane Garcia <rory.garcia@us.idemia.com>
2026-02-08 00:41:43 -0500

    remove node modules

--- apps/api/dist/enums/project-type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProjectType = void 0;
-var ProjectType;
-(function (ProjectType) {
-    ProjectType[ProjectType["Feature"] = 0] = "Feature";
-    ProjectType[ProjectType["Short"] = 1] = "Short";
-    ProjectType[ProjectType["Television"] = 2] = "Television";
-})(ProjectType || (exports.ProjectType = ProjectType = {}));
